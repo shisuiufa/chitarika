6d10d3916316f0f5a7171d57b29ed92393c1698c
Hacker <romanrus98@mail.ru>
2026-06-22 16:10:22 +0500

    refactor electron

--- src/shared/lib/electron/quit-app.ts
+++ src/shared/lib/electron/quit-app.ts
@@ -1,9 +1,5 @@
 export async function quitApp() {
-  console.log("quitApp called");
-  console.log("window.electronApp:", window.electronApp);
-
   if (!window.electronApp) {
-    console.error("window.electronApp is undefined");
     return;
   }
 
